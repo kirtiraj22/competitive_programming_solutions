1830f0d20ee45feaf9f64c7017fc4458bcc59397
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2021-12-27 00:00:40 +0530

    Create find_remainder_CC.cpp
    QUESTION LINK : https://www.codechef.com/problems/FLOW002
    
    Write a program to find the remainder when an integer A is divided by an integer B.
    
    SAMPLE INPUT
    3 
    1 2
    100 200
    40 15
    
    SAMPLE OUTPUT:
    100
    10

---
+++ find_remainder_CC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int T,num1,num2;
+        cin >> T;
+        while(T--){
+            cin >> num1;
+            cin >> num2;
+            cout << num1%num2<<endl;
+        }
+}
