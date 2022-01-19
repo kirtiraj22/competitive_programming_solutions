a09f550f0faa30eb4d3075c23608e2816e6392a2
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-01-19 16:42:19 +0530

    Create helpingchefCC.cpp
    Question link : https://www.codechef.com/problems/FLOW008
    
    //HELPING CHEF : CODECHEF(PROBLEM CODE = FLOW008)
    //WAP which takes input and displays "Thanks for helping chef!" if number is less than 10 else prints "-1".
    
    ->constrains:
    	     1 ≤ T ≤ 1000
    	     -20 ≤ N ≤ 20	
    
    -->input format:
    	-first line contains T test cases.
    	-second line contains N integers.
    
    SAMPLE INPUT:
    3 
    1
    12
    -5
    
    SAMPLE OUTPUT:
    Thanks for helping Chef!
    -1
    Thanks for helping Chef!

---
+++ helpingchefCC.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,N;
+	cin >> T;
+	while(T--){
+		cin >> N;
+		if(N<10){
+			cout << "Thanks for helping Chef!"<<endl;
+			}
+		else{
+			cout <<"-1"<<endl;
+			}
+		}
+	return 0;
+}
