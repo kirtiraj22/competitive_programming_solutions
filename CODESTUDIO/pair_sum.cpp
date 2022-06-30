95350062a2013a614a363409ac21e85f3585abf9
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-06-30 16:47:36 +0530

    Update pair_sum.cpp

--- CODESTUDIO/pair_sum.cpp
+++ CODESTUDIO/pair_sum.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h> 
 vector<vector<int>> pairSum(vector<int> &arr, int s){
-   vector<vector<int>> v;
+    vector<vector<int> > ans;
    for(int i = 0; i < arr.size(); i++){
        for(int j = i + 1; j < arr.size(); j++){
-           vector <int> temp;
            if(arr[i]+arr[j] == s){
+               vector <int> temp;
                temp.push_back(min(arr[i],arr[j]));
                temp.push_back(max(arr[i],arr[j]));
-               v.push_back(temp);
+               ans.push_back(temp);
            }
-           
        }
+       
    }
-    sort(v.begin(),v.end());
-    return v;
+    sort(ans.begin(),ans.end());
+    return ans;
 }
