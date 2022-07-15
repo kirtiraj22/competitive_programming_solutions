8e31b7e7e3bd3071a970fec944228b7d48834989
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-07-15 11:29:29 +0530

    Create YES_OR_YES.cpp
    Question link : https://codeforces.com/contest/1703/problem/A

---
+++ CODEFORCES/YES_OR_YES.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        int count = 0;
+
+        if(s[0] == 'Y' || s[0] == 'y'){
+            count++;
+        }
+        if(s[1] == 'E' || s[1] == 'e'){
+            count++;
+        }
+        if(s[2] == 'S' || s[2] == 's'){
+            count++;
+        }
+
+        if(count == 3){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
